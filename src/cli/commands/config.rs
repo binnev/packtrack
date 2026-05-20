983a46186a77644897d43bb539d1373ab828741e
binnev <robin.m.neville@gmail.com>
2026-05-20 02:19:09 +0200

    fix(CLI): config update now displays updated values
    closes #23

--- src/cli/commands/config.rs
+++ src/cli/commands/config.rs
@@ -17,16 +17,19 @@ pub fn handle_config_command(
     command: ConfigCommand,
     settings_manager: &mut FileSettingsManager,
 ) -> Result<()> {
-    let settings = &mut settings_manager.settings;
     match command {
-        ConfigCommand::List => display_settings(settings)?,
+        ConfigCommand::List => display_settings(&settings_manager.settings)?,
         ConfigCommand::Set { key, value } => {
-            settings.update(&key, value)?;
+            settings_manager
+                .settings
+                .update(&key, value)?;
             settings_manager.save()?;
+            display_settings(&settings_manager.settings)?;
         }
         ConfigCommand::Reset => {
             settings_manager.settings = Settings::default()?;
             settings_manager.save()?;
+            display_settings(&settings_manager.settings)?;
         }
     }
     Ok(())
